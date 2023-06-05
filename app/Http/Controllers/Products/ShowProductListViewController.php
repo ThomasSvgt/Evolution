9f76e29a5427a9cf3e77c66a2cc8f86078fca21e
Yoann TANIERE <ytaniere@pageup.fr>
2023-06-05 16:31:28 +0200

    Ajout de la datatables de produits, et des filtres WIP

--- app/Http/Controllers/Products/ShowProductListViewController.php
+++ app/Http/Controllers/Products/ShowProductListViewController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers\Products;
 
+use App\Facades\Pagination;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -10,10 +15,35 @@ class ShowProductListViewController
     /**
      * @return Response
      */
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
         return Inertia::render('products/ProductList', [
-
+            'datatable' => fn() => $this->getProducts($request)
         ]);
+    }
+
+    private function getProducts(Request $request)
+    {
+        $input = Validator::validate($request->all(), [
+            'search' => ['sometimes', 'nullable', 'string'],
+            'filters' => ['sometimes', 'nullable', 'array'],
+        ]);
+
+        $search = $input['search'] ?? null;
+        $price = $input['filters']['prices']['value'] ?? null;
+
+        $query = DB::table('products as p')
+            ->whereSearch($search, ['p.name', 'p.description'])
+            ->when($price !== null, function (Builder $query) use ($price) {
+                $query->where('p.price', '=', $price);
+            })
+            ->select([
+                'id',
+                'name',
+                'price',
+                'description'
+            ]);
+
+        return Pagination::get($request, $query);
     }
 }
--- resources/js/pages/products/ProductList.vue
+++ resources/js/pages/products/ProductList.vue
@@ -11,6 +11,7 @@
             <div class="row flex-center card-container">
                 <q-card class="my-card col-11">
                     <q-card-section>
+
                         <div class="row">
                             <div class="text-bold text-grey-8 col-auto" style="margin-top: 10px">
                                 {{ $t('repositories.selected_repository') }}
@@ -19,6 +20,47 @@
                                 <repository-selector :repository="repository"/>
                             </div>
                         </div>
+
+                        <div class="row" style="padding-top: 40px">
+                            <div class="text-bold text-grey-8 col-auto" style="margin-top: 10px">
+                                Filtres
+                            </div>
+                            <div class="col-3" style="margin-left: 30px">
+                                <q-input outlined dense debounce="300" v-model="search" placeholder="Recherche">
+                                    <template v-slot:append>
+                                        <q-icon name="search" />
+                                    </template>
+                                </q-input>
+                            </div>
+                            <div class="col-2" style="margin-left: 30px">
+                                <q-input outlined dense v-model="filters.prices"
+                                          @update:model-value="reloadDatatable"
+                                          option-label="text"
+                                          option-value="value"
+                                          clearable
+                                          label="Prix">
+                                </q-input>
+                            </div>
+                        </div>
+
+                    </q-card-section>
+
+                    <q-card-section>
+
+                        <q-table
+                            v-model:pagination="pagination"
+                            title="Produits"
+                            :columns="headers"
+                            :rows="items"
+                            :loading="loading"
+                            :filter="search"
+                            row-key="id"
+                            selection="multiple"
+                            v-model:selected="selected"
+                            @request="onRequest">
+
+                        </q-table>
+
                     </q-card-section>
                 </q-card>
             </div>
@@ -31,8 +73,107 @@
 
 import ViewLayout from "@/components/ViewLayout.vue";
 import RepositorySelector from "@/components/repositories/RepositorySelector.vue";
+import {computed, ref} from "vue";
+import {router} from "@inertiajs/vue3";
 
 let repository = {value: 'products', text :'Produits'}
+
+let props = defineProps({
+    datatable: {
+        type: Object,
+        required: true,
+    }
+})
+
+let items = computed(() => {
+    return props.datatable?.items
+})
+
+let total = computed(() => {
+    return props.datatable?.total;
+})
+
+let headers = ref([
+    {
+        name: 'name',
+        align: 'left',
+        label: 'Nom',
+        field: 'name',
+        sortable: true,
+        style: 'min-width: 200px; max-width:200px'
+    },
+    {
+        name: 'price',
+        align: 'left',
+        label: 'Prix',
+        field: 'price',
+        sortable: true,
+        style: 'min-width: 200px; max-width:200px'
+    },
+    {
+        name: 'description',
+        align: 'left',
+        label: 'Description',
+        field: 'description',
+        sortable: true,
+        style: 'min-width: 200px; max-width:200px'
+    },
+    {
+        name: 'actions',
+        align: 'right',
+        label: 'Actions',
+        sortable: false,
+        style: 'min-width: 300px; max-width:150px'
+    },
+]);
+
+const pagination = ref({
+    sortBy: 'name',
+    descending: false,
+    page: 1,
+    rowsPerPage: 10,
+    rowsNumber: null
+})
+
+const filters = ref({})
+
+let search = ref('')
+
+const loading = ref(false)
+
+let selected = ref([])
+
+function syncPagination(props){
+    pagination.value = props.pagination;
+}
+
+function onRequest (props) {
+    syncPagination(props)
+    reloadDatatable()
+}
+
+function startLoading(){
+    loading.value = true
+}
+
+function stopLoading(){
+    loading.value = false
+}
+
+function reloadDatatable(){
+    startLoading()
+    let data = {...pagination.value, search : search.value, filters : filters.value}
+    router.get('/repositories/products', data, {
+        preserveState: true,
+        preserveScroll: true,
+        replace: true,
+        only: ['datatable'],
+        onFinish: () => {
+            stopLoading()
+            pagination.value.rowsNumber = total;
+        }
+    })
+}
 
 </script>
 
